4183775aa4026259e839a531b6d0017c8df9ec59
Nietises <Nietises@outlook.com>
2022-12-05 17:59:28 +0800

    移除动态页广告和热门模块

--- Bilibili.js
+++ Bilibili.js
@@ -4,32 +4,45 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Nietises
-// @match        https://t.bilibili.com/
+// @match        https://t.bilibili.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    window.onload=function(){
-        var tmpArry = null;
-        var ms = 0; // 定时器用时
-        var timer = setInterval(()=>{
-            ms+=10;
-            tmpArry = document.querySelectorAll("section")
-            if(tmpArry.length>6){
-                console.log("定时器用时:"+ms);
-                rm();
-                clearInterval(timer);
-            }
-        },10)
-
-        function rm(){
-            tmpArry[6].remove();
-            delete tmpArry[6];tmpArry[6]=null
-            if(tmpArry[6] != undefined){
-                tmpArry[6].remove();
-            }
-        }
+// ==快速查找元素工具==================================================================
+function el(e) {
+    let a = document.querySelectorAll(e);
+    if (a.length == 0) {
+        return 0;
     }
-})();
+    if (a.length == 1) {
+        return a[0];
+    } else {
+        return a;
+    }
+}
+
+window.onload = main();
+
+function main() {
+    var ms = 0;
+
+    var timer = setInterval(() => {
+        ms += 6;
+        var ad = el(".bili-dyn-ads")
+        var topic = el(".topic-panel").parentNode
+
+        if (topic && ad) {
+            console.log("time:" + ms);
+
+            console.log("ad", ad);
+            console.log("topic", topic);
+
+            ad.remove()
+            topic.remove()
+            clearInterval(timer);
+            timer = null;
+        }
+
+    }, 6)
+}
